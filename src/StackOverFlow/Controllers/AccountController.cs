dac58df9f99954da70779433e0e82fc96c4bb687
David Rolfs and Hunter Parks <david+hunter@>
2017-08-07 11:09:24 -0700

    add bootstrap to our project

--- src/StackOverFlow/Controllers/AccountController.cs
+++ src/StackOverFlow/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using StackOverFlow.Models;
+using StackOverFlow.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
