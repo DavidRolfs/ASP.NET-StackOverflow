010a9a7783d32f6384b10bb9bf06151e203d6101
David Rolfs and Hunter Parks <david+hunter@>
2017-08-07 10:10:48 -0700

    add IdentityUser to ApplicationUser Model

--- src/StackOverFlow/Models/ApplicationUser.cs
+++ src/StackOverFlow/Models/ApplicationUser.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace StackOverFlow.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
     }
 }
